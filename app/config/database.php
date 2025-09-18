df401f85a1ecc2149c808698fd1e7cdbf656dc9c
Ryu Andrew Franque <ryuandrewfraqnue@gmail.com>
2025-09-18 23:08:31 +0800

    update db config for render

--- app/config/database.php
+++ app/config/database.php
@@ -58,15 +58,14 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
-    'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
-    'charset'	=> 'utf8mb4',
-    'dbprefix'	=> '',
-    // Optional for SQLite
+    'driver'    => 'mysql',
+    'hostname'  => getenv('DB_HOST'),
+    'port'      => getenv('DB_PORT'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
+    'database'  => getenv('DB_NAME'),
+    'charset'   => 'utf8mb4',
+    'dbprefix'  => '',
     'path'      => ''
 );
 
